fce015cb47586a7aeff4bb3f92d4c4da94e63263
Abhishek Jain <abhishek.jain@av-mbp-913.sanjuan.avalara.com>
2020-08-26 18:10:23 +0530

    added spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
-export class Users extends Component {
-    
-    render() {
-        return (
-            <div style={userStyle}>
-                {this.props.users.map(user => (
-                    <UserItem key={user.id} user={user} />
-                ))}
-            </div>
-        )
-    }
+const Users = ({users, loading}) => {
+        if(loading) {
+            return <Spinner />
+        } else{
+            return (
+                <div style={userStyle}>
+                    {users.map(user => (
+                        <UserItem key={user.id} user={user} />
+                    ))}
+                </div>
+            )
+        }
 }
 
 const userStyle = {
